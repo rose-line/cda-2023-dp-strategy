236d5a50bcb173c070f105199b5958c04069c4bb
rose-line <1886792+rose-line@users.noreply.github.com>
2023-03-31 09:41:12 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Strategy.Business.Models;
+using System;
+
+namespace Strategy
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var order = new Order
+      {
+        ShippingDetails = new ShippingDetails
+        {
+          OriginCountry = "Luxembourg",
+          DestinationCountry = "Luxembourg"
+        }
+      };
+
+      order.LineItems.Add(new Item("01234", "C# dans ta face", 100m, ItemType.Literature), 1);
+      order.LineItems.Add(new Item("56780", "Consulting Cybersécurité", 100m, ItemType.Service), 1);
+
+      Console.WriteLine(order.GetTax());
+    }
+  }
+}
